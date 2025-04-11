962cdfd1c8665d0be59a58a68818d38a4bfe3f54
Sahib17 <sharp.sahib@gmail.com>
2025-04-11 12:19:18 -0700

    minor fixes

--- src/main/HotelManagementApp.java
+++ src/main/HotelManagementApp.java
@@ -1,10 +1,17 @@
 package main;
 
 import db.DBInitializer;
+import ui.LoginScreen;
+
+import javax.swing.*;
 
 public class HotelManagementApp {
     public static void main(String[] args) {
         DBInitializer.initialize();
-        System.out.println("🏨 Hotel Management System is ready to go!");
+
+        // Launch login screen on the Event Dispatch Thread
+        SwingUtilities.invokeLater(() -> {
+            new LoginScreen().setVisible(true);
+        });
     }
 }
